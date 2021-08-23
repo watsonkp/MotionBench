e5b20c819619348d5faabf08688d68dff35571d0
Sullied Security <sulliedeclat@gmail.com>
2021-08-23 07:59:56 -0400

    Initial Commit

---
+++ Motion/MotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionApp.swift
+//  Motion
+//
+//  Created by Kevin Watson on 2021-08-23.
+//
+
+import SwiftUI
+
+@main
+struct MotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
